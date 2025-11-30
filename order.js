4c506084dae1b8e786902a6d375d500417c2ff6f
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-11-30 19:58:16 +0200

    Template Created

--- order.js
+++ order.js
@@ -1,14 +1,20 @@
 const ordersBodyActive = document.getElementById("ordersBodyActive");
-
-if (ordersBodyActive) ordersBodyActive.innerHTML = "";
-
 const popup = document.getElementById("popup");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 
+//Empty Table
+if (ordersBodyActive) ordersBodyActive.innerHTML = "";
+
+//Popup
 openBtn.onclick = () => popup.style.display = "block";
 closeBtn.onclick = () => popup.style.display = "none";
 
 window.onclick = (e) => {
     if (e.target === popup) popup.style.display = "none";
 };
+
+//Create Order
+function NewOrder(order) {
+    const temp
+}
