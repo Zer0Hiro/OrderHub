854da3bd8aa064ad8df03b44dc99e89dc0d0976c
TomerPine <tomer.pine1@gmail.com>
2025-12-01 16:44:39 +0200

    finished the simulate order function

--- random.js
+++ random.js
@@ -4,14 +4,37 @@ function chooseRandomOrder(a) {
     return a[randIndex];
 }
 
-
-
 function simulateNewOrder() {
+    // adds a new simulated order to the incoming orders list
     let orders = [
-    { Provider: "Wolt", ID: "WO-1389", customer: "John", items: { Pizza: 2, Coke: 2}, sotal: 50, status: "pending" },
-    { Provider: "Mishloha", ID: "MS-1399", customer: "John", items: { Burger: 1}, sotal: 500, status: "pending" }
+    { provider: "Wolt", ID: "WO-1389", customer: "John", items: "Pizza: 2, Coke: 1", total: 50 },
+    { provider: "Mishloha", ID: "MS-1399", customer: "Leo", items: "Burger: 10", total: 500 }
 
     ]   
-    const newOrder = chooseRandomOrder(orders);
-    NewOrder(newOrder);
+    const tableBody = document.getElementById("ordersBodyActive");
+    const rowTemplate = document.getElementById("orderRowTemplate");
+    const newRow = document.importNode(rowTemplate.content, true);
+    const cells = newRow.querySelectorAll('td');
+    const simOrder = chooseRandomOrder(orders);
+    // get the curr time as a string: `timeString`
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', { 
+        hour: '2-digit', 
+        minute: '2-digit',
+        hour12: false 
+    });
+
+    const orderId = simOrder.provider + "-" + Date.now().toString().slice(-6);
+    
+    cells[0].textContent = timeString;
+    cells[1].textContent = simOrder.provider;
+    cells[2].textContent = orderId;
+    cells[3].textContent = simOrder.customer;
+    cells[4].textContent = simOrder.items;
+    cells[5].textContent = simOrder.total;
+    cells[6].textContent = "Pending";
+
+     tableBody.appendChild(newRow);
+
+
 }
