0ee38c24e16edf822f71c92c49d9f9aa8135ad95
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-12-01 11:16:59 +0200

    Merged JS

--- order.js
+++ order.js
@@ -9,7 +9,19 @@ const orderAddress = document.getElementById("orderAddress");
 const orderTotal = document.getElementById("orderTotal");
 const orderPayment = document.getElementById("orderPayment");
 
+//Random Orders
+function chooseRandomOrder(a) {
+    // Returns a random order from list a.
+    const randIndex = Math.floor(Math.random() * a.length); // Math.random() returns  a float between 0 to 1
+    return a[randIndex];
+}
 
+let orders = [
+    { Provider: "Wolt", ID: "WO-1389", Customer: "John", items: { Pizza: 2, Coke: 2}, Total: 50, Status: "pending" },
+    { Provider: "Mishloha", ID: "MS-1399", Customer: "John", items: { Burger: 1}, Total: 500, status: "pending" }
+
+
+]
 
 //Empty Table
 if (ordersBodyActive) ordersBodyActive.innerHTML = "";
@@ -36,13 +48,14 @@ function cleanForm()
 }
 
 //Create Order
-function NewOrder(order) {
+function NewOrder() {
     const tableBody = document.getElementById("ordersBodyActive");
     const rowTemplate = document.getElementById("orderRowTemplate");
-
+    
     const newRow = document.importNode(rowTemplate.content, true);
     const cells = newRow.querySelectorAll('td');
-
+    
+    
     // Generate current time
     const now = new Date();
     const timeString = now.toLocaleTimeString('en-US', { 
@@ -53,7 +66,7 @@ function NewOrder(order) {
     
     // Generate order ID
     const orderId = "MAN-" + Date.now().toString().slice(-6);
-
+    
     cells[0].textContent = timeString;
     cells[1].textContent = "Manual";
     cells[2].textContent = orderId;
@@ -61,11 +74,33 @@ function NewOrder(order) {
     cells[4].textContent = orderItems.value;
     cells[5].textContent = orderTotal.value;
     cells[6].textContent = "Pending";
-
-
+    
+    
     tableBody.appendChild(newRow);
-//    popup.style.display = "none";
-
+    popup.style.display = "none";
+    
     // Clear form fields
     cleanForm();
+}
+
+// Form Validation
+function validateForm() {
+    var phone = document.getElementById('orderTel').value;
+    var items = document.getElementById('orderItems').value;
+    var amount = document.getElementById('orderTotal').value;
+
+    var alertms = ''
+    if(trim(phone).length != 10) {
+        alertms = alertms + "Please enter 10 digits phone number\n";
+    }
+
+    if(amount == 0 && trim(items).length != 0) {
+        alertms = alertms + "False Amount";
+    }
+
+    if(alertms != ''){
+        alert(alertms);
+    }
+
+    else {NewOrder();}
 }
