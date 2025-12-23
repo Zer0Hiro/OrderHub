b15c19bca792cd79143a63e4953b11b65ec902dd
JacoobJacoob <yakovfumm@gmail.com>
2025-12-23 11:25:25 +0200

    cleaned up repetetive code

--- order.js
+++ order.js
@@ -21,6 +21,12 @@ const filterStatus = document.getElementById("statusFilter");
 //all tables
 const main = document.getElementById("main");
 
+//TABS
+const incTable = document.getElementById("incTable");
+const cancelledTable = document.getElementById("cancelledTable");
+const completedTable = document.getElementById("completedTable");
+
+
 //Empty Tables
 if (ordersBodyActive) ordersBodyActive.innerHTML = "";
 if (ordersBodyCancelled) ordersBodyCancelled.innerHTML = "";
@@ -138,7 +144,6 @@ function simulateNewOrder() {
 
     saveOrderToStorage(tempOrder); // Sends the new order to "saveOrderToStorage" in dn.js
 
-    cleanTables();
     loadOrders();
 
 }
@@ -169,7 +174,6 @@ function NewOrder() {
 
     saveOrderToStorage(newOrder); // Sends the new order to "saveOrderToStorage" in dn.js
 
-    cleanTables();
     loadOrders();
 
     changeDisplay(1);
@@ -235,8 +239,6 @@ function statusBtn(cells, row, order) {
 function statusBtnClick(order, status) {
     order.status = status;
     updateOrderInStorage(order);
-
-    cleanTables();
     loadOrders();
 }
 
@@ -306,32 +308,27 @@ function openOrderDeatil(row, order, cells) {
 }
 
 // tabs:
-incTabBtn.onclick = () => {
-    incTable.style.display = "block";
-    cancelledTable.style.display = "none";
-    completedTable.style.display = "none";
+incTabBtn.onclick = () => chooseTable(incTable);
+
+CancelTabBtn.onclick = () => chooseTable(cancelledTable);
+
+CompTabBtn.onclick = () =>  chooseTable(completedTable);
+
+function chooseTable(table)
+{
+    table.style.display = "block";
+    for(var tables of [incTable, cancelledTable, completedTable])
+    {
+        if(tables != table) tables.style.display = "none";
+    }
 }
 
-CancelTabBtn.onclick = () => {
-    incTable.style.display = "none";
-    cancelledTable.style.display = "block";
-    completedTable.style.display = "none";
-}
 
-CompTabBtn.onclick = () => {
-    incTable.style.display = "none";
-    cancelledTable.style.display = "none";
-    completedTable.style.display = "block";
-}
-
-function filter() {
-    cleanTables();
-    loadOrders();
-}
 
 // Function for loading saved data from storage into the page
 function loadOrders() {
-
+    //you have to clean the tables to load them clean
+    cleanTables();
     // Retrieves the data array from local storage
     const provider = filterProvider.value;
     const orders = getOrdersFromStorage().filter(order => (order.provider == provider || provider == "all"));
